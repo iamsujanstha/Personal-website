35d9d287e14a67339edd0d94a1a092d37ffcc9fe
Sujan <sujan@aerion.com.au>
2023-09-02 06:38:38 +0545

    FIX: Scroll to top at bottom

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,23 @@ import ScrollToTop from "./components/ScrollToTop";
 const App = () => {
   return (
     <BrowserRouter>
-      <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
         </div>
-        <ScrollToTop />
         <About />
         <Experience />
         <Tech />
         <Works />
-        <div className='relative z-0'>
+        <div className="relative z-0">
           <Contact />
           <StarsCanvas />
         </div>
+        <ScrollToTop />
       </div>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
